b59fcd4ec97bd8b72f834c88ba4f5a580fb5e1df
mbachmann <m.bachmann@united.ch>
2025-02-25 12:18:20 +0100

    add new methods

--- src/main/java/com/example/todo/repository/TodoListNameRepository.java
+++ src/main/java/com/example/todo/repository/TodoListNameRepository.java
@@ -5,9 +5,11 @@ import com.example.todo.model.TodoListName;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface TodoListNameRepository extends JpaRepository<TodoListName, UUID> {
 
+    List<TodoListName> findByName(String name);
 }
