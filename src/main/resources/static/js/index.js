57dba48966f63ba0a6bf6689bdbae100dcb6b2f5
mbachmann <m.bachmann@united.ch>
2025-03-30 13:03:50 +0200

    add google css2 lib use own guid function

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -40,7 +40,7 @@ function postNewTodoList(listName) {
             headers: {
                 "Content-Type": "application/json"
             },
-            data: JSON.stringify({id: crypto.randomUUID(), name: listName}),
+            data: JSON.stringify({id: getUUID(), name: listName}),
             success: function (data) {
                 loadLists();
             },
@@ -194,4 +194,32 @@ function getAndCreateTodoListRow(listId) {
         let oldListItem = $("#item" + data.listId);
         oldListItem.replaceWith(newListItem);
     });
+}
+
+function getUUID() {
+    var result = '';
+    var hexcodes = "0123456789abcdef".split("");
+
+    for (var index = 0; index < 32; index++) {
+        var value = Math.floor(Math.random() * 16);
+
+        switch (index) {
+            case 8:
+                result += '-';
+                break;
+            case 12:
+                value = 4;
+                result += '-';
+                break;
+            case 16:
+                value = value & 3 | 8;
+                result += '-';
+                break;
+            case 20:
+                result += '-';
+                break;
+        }
+        result += hexcodes[value];
+    }
+    return result;
 }
